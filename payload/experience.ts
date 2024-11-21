f1e6277c006875857da334da17de1866729dedcc
nkpark@agilesoda.ai <david@davidui-MacBookAir.local>
2024-11-21 22:36:50 +0900

    update

--- payload/experience.ts
+++ payload/experience.ts
@@ -5,11 +5,11 @@ const experience: IExperience.Payload = {
   disableTotalPeriod: false,
   list: [
     {
-      title: 'Linux Foundation',
+      title: '애자일소다(AgileSoda)',
       positions: [
         {
-          title: 'System Engineer',
-          startedAt: '1996-05',
+          title: 'DataEngineer',
+          startedAt: '2023-02',
           descriptions: [
             'Steering Kernel patching and updates for various distributions',
             'Automating deployment and integration processes for Linux servers',
@@ -19,9 +19,9 @@ const experience: IExperience.Payload = {
           skillKeywords: ['C', 'Linux', 'Kernel', 'Security', 'Automation'],
         },
         {
-          title: 'System Engineer Intern',
-          startedAt: '1996-02',
-          endedAt: '1996-05',
+          title: 'MLOps Engineer',
+          startedAt: '2021-08',
+          endedAt: '2023-02',
           descriptions: [
             'Assisted senior engineers in maintaining and updating kernel patches for Linux distributions',
             'Supported deployment and integration tasks for Linux server environments',
@@ -29,24 +29,25 @@ const experience: IExperience.Payload = {
           ],
           skillKeywords: ['C', 'Linux', 'Kernel', 'Support', 'Research'],
         },
+
       ],
     },
-    {
-      title: 'Open Source Community',
-      positions: [
-        {
-          title: 'System Advocate and Developer',
-          startedAt: '2010-06',
-          endedAt: '2018-01',
-          descriptions: [
-            'Promoted the use and development of Linux globally.',
-            'Developed automated scripts to streamline system configuration.',
-            'Facilitated live patching systems, reducing downtime.',
-          ],
-          skillKeywords: ['Open Source', 'Linux', 'AWS', 'RDBMS'],
-        },
-      ],
-    },
+    // {
+    //   title: 'Open Source Community',
+    //   positions: [
+    //     {
+    //       title: 'System Advocate and Developer',
+    //       startedAt: '2010-06',
+    //       endedAt: '2018-01',
+    //       descriptions: [
+    //         'Promoted the use and development of Linux globally.',
+    //         'Developed automated scripts to streamline system configuration.',
+    //         'Facilitated live patching systems, reducing downtime.',
+    //       ],
+    //       skillKeywords: ['Open Source', 'Linux', 'AWS', 'RDBMS'],
+    //     },
+    //   ],
+    // },
   ],
 };
 
